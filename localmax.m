0b347d0f2fe162d94db1b955d0f1a25b6994014e
Nathan Dauber <Nathan.Dauber@gmail.com>
2012-04-08 11:47:16 -0500

    have a function that will determine the localmaximum in an matrix

---
+++ localmax.m
@@ -0,0 +1,14 @@
+function [maximas] = localmax(matrix)   
+    
+    bw = matrix > imdilate(matrix,[1 1 1; 1 0 1; 1 1 1]);
+    maximas = zeros(sum(sum(bw)), 1);
+    index = 1;
+    for ix = 1:size(bw,1)
+        for iy = 1:size(bw, 2)
+            if (bw(ix,iy) > 0)
+                maximas(index) = (ix - 1)*size(bw,1) + iy;
+                index = index + 1;
+            end
+        end
+    end
+end

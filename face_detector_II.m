424f2fdbebe54eef8df275f2d0d632a792bf7aa4
Nathan Dauber <Nathan.Dauber@gmail.com>
2012-04-10 23:14:50 -0500

    I have the integral image version retraining and displaying most of the requested info, still need time info

--- face_detector_II.m
+++ face_detector_II.m
@@ -27,127 +27,27 @@ desiredOut = [ones(1,numFaces) -ones(1,numNonFaces)]';
 weights = [numNonFaces.*ones(1,numFaces) numFaces.*ones(1,numNonFaces)]';
 weights = weights./sum(weights(:));
 %% now, make 20 Features
-% initial some variables
-FINALFEAT_II = zeros(2,8,100);
-FINALTHRESH = zeros(1,100);
-bests = [];
-startClock = clock;
-for numFeats = 1:100
-    bestWeakClassifierScore = 0;    
-    for jx = 1:20  
-      
-        [POSITIVE NEGATIVE] = gen_interval_feature;         
-        %generate_feature and have it return the corners of positive regions, and corners of negative regions 
-        %score of face = (sum up positive - sum of negative regions) 
-        
-        scores = zeros(1, 1, numFaces + numNonFaces);
-        for px = 1 : size(POSITIVE,1)
-            points = POSITIVE(px, 1:4);
-            row1 = points(1);
-            row2 = points(3);
-            col1 = points(2);
-            col2 = points(4);
-            scores = scores + (allFaces(row1, col1 ,:) + allFaces(row2, col2 ,:) - allFaces(row1, col2, :) - allFaces(row2, col1, :) );
-        end
-        for px = 1 : size(NEGATIVE,1)
-            points = NEGATIVE(px, 1:4);
-            row1 = points(1);
-            row2 = points(3);
-            col1 = points(2);
-            col2 = points(4);
-            scores = scores - (allFaces(row1, col1 ,:) + allFaces(row2, col2 ,:) - allFaces(row1, col2, :) - allFaces(row2, col1, :)) ;
-        end
-        scores = reshape(scores,numFaces+numNonFaces,1);
-        
-        % now try different thresholds.
-        thresholdList = linspace(min(scores),max(scores),1000);  % make 1000 thresholds.
-        cScore = 0;    %initialize some stuff about those thresholds.
-        cThresh = 0;
-        for ix = 1:1000
-            % compute classification result with this threshold
-            classifierResult = sign(scores-thresholdList(ix));
-            
-            % compute "weighted" score for each face.
-            tmp = classifierResult .* desiredOut .* weights;
-            
-            % classifier score is the sum of these weighted scores.
-            tmpScore = sum(tmp);
-            if tmpScore > cScore   % if it is better, set this threshold, score as current best...
-                cThresh = thresholdList(ix);
-                cScore = tmpScore;
-            end
-        end
-        
-        % go back and get classification for the best threshold you found.
-        weakClassifier = sign(scores-cThresh);
-        
-        % recompute how good that actually was.
-        weakClassifierScore = sum(weakClassifier .* desiredOut .*weights);
-        
-        % if better than we've seen so far, then save it.
-        if weakClassifierScore > bestWeakClassifierScore
-            bestWeakClassifierScore = weakClassifierScore;
-            FINALFEAT_II(1, 1:size(POSITIVE,1)*size(POSITIVE,2), numFeats) = POSITIVE(:);
-            FINALFEAT_II(2, 1:size(NEGATIVE,1)*size(NEGATIVE,2), numFeats) = NEGATIVE(:);
-            FINALTHRESH(numFeats) = cThresh;
-        end
-    end
-    bests(numFeats) = bestWeakClassifierScore;
-    
-    % ok... so the above loop picked the best of 100 possible features.
-    % now, let's update the weights of the samples.
-    weights = weights .* exp(-desiredOut .* weakClassifier);
-    
-    % normalize the weights or they'll go crazy.
-    weights = weights./sum(weights(:));
-end
+[FINALFEAT_II FINALTHRESH featureRanking] = train_classifiers_II(allFaces, desiredOut, weights, numFaces, numNonFaces);
+calc_score_II(allFaces,desiredOut, FINALFEAT_II, FINALTHRESH, featureRanking, numFaces, numNonFaces);
 
-startClock =  clock - startClock;
-fprintf('finished training %d mins %d secs for #%d faces \r', startClock(5), startClock(6), size(allFaces,3));
+%% now retain on everything but the last 100, and test against those last 100
+numFaces = size(faces,3) - 100;
+numNonFaces = size(nonfaces,3) - 100;
+allFaces = zeros(24,24, numFaces + numNonFaces);
+allFaces(:,:,1:numFaces) = facesII(:,:,1:numFaces);
+allFaces(:,:,numFaces+1:numFaces+numNonFaces) = nonfacesII(:,:,1:numNonFaces);
 
-%%
-[y i] = sort(bests,2,'descend');
-AS = zeros(100,1, numFaces + numNonFaces);
-for fx = 1:100
-    POSITIVE = reshape(FINALFEAT_II(1,:,fx),4,2)';        
-    NEGATIVE = reshape(FINALFEAT_II(2,:,fx),4,2)';
-    for px = 1 : 2
-        points = POSITIVE(px, 1:4);
-        row1 = points(1);
-        row2 = points(3);
-        col1 = points(2);
-        col2 = points(4);
-        if (row1 > 0 && row2 > 0)
-            AS(fx,1,:) = AS(fx,1,:) + (allFaces(row1, col1 ,:) + allFaces(row2, col2 ,:) - allFaces(row1, col2, :) - allFaces(row2, col1, :) );
-        end
-    end
-    for px = 1 : 2
-        points = NEGATIVE(px, 1:4);
-        row1 = points(1);
-        row2 = points(3);
-        col1 = points(2);
-        col2 = points(4);
-        if (row1 > 0 && row2 > 0)
-            AS(fx,1,:) = AS(fx,1,:) - (allFaces(row1, col1 ,:) + allFaces(row2, col2 ,:) - allFaces(row1, col2, :) - allFaces(row2, col1, :)) ;
-        end
-    end
-end
-beep
+desiredOut = [ones(1,numFaces) -ones(1,numNonFaces)]';
+% make the total weight of faces and non faces the same (so that just
+% calling everything "not a face" isn't a win...
+weights = [numNonFaces.*ones(1,numFaces) numFaces.*ones(1,numNonFaces)]';
+weights = weights./sum(weights(:));
+[FINALFEAT_II FINALTHRESH featureRanking] = train_classifiers_II(allFaces, desiredOut, weights, numFaces, numNonFaces);
 
-AS = reshape(AS, 100, numFaces+numNonFaces);
-
-%AS = FF'*allFaces;                      % Compute the score of every face with every feature.
-AT = repmat(FINALTHRESH',1,size(AS,2)); % create matrix of all thresholds, replicating it so its same size as AS
-VOTES = sign(AS - AT);                 % compute weak classification  of all faces for all features
-CLASSIFICATION = sign(sum(VOTES)-eps);  % sum the classifications.  if something has EXACTLY the same number of 
-                                        % yes and no votes, then it is 0
-                                        % instead of -1 of +1, so -eps
-                                        % makes sure that doesn't happen.                                      
-                                        
-fprintf('true positive: %d %% \n',sum(CLASSIFICATION' == 1 &  desiredOut == 1)/numFaces);
-fprintf('true negative: %d %% \n', sum(CLASSIFICATION' == -1 &  desiredOut == -1)/numNonFaces);
-fprintf('false negatives: %d %% \n',sum(CLASSIFICATION' == -1 &  desiredOut == 1)/numFaces);
-fprintf('false positives: %d %% \n', sum(CLASSIFICATION' == 1 &  desiredOut == -1)/numNonFaces);
+allFaces = zeros(24,24, 200);
+allFaces(:,:,1:100) = facesII(:,:,numFaces+1:numFaces+100);
+allFaces(:,:,101:200) = nonfacesII(:,:,numNonFaces+1:numNonFaces+100);
+calc_score_II(allFaces,[ones(1,100) -ones(1,100)]', FINALFEAT_II, FINALTHRESH, featureRanking, 100, 100);
 
 %% classify an image
 %image = rgb2gray(imresize(imread('http://i.imgur.com/02npE.jpg'),.5));
